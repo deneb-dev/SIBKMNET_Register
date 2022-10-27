656a49299014ba0ebcb301956ee22cc7ade93324
deneb-dev <deneb288@gmail.com>
2022-10-27 15:51:34 +0800

    Tugas Hashing .NET

---
+++ NETSIBKM2_WebApp/ViewModels/Update.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETSIBKM2_WebApp.ViewModels
+{
+    public class Update
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string OldPw { get; set; }
+
+        public string NewPw { get; set; }
+    }
+}
